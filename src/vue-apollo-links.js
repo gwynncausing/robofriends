e62914888ed28c94436915cf2fb2e89affb57dad
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2021-05-12 01:04:52 +0800

    Fix bug eslint
    npm run lint -- --fix

--- src/vue-apollo-links.js
+++ src/vue-apollo-links.js
@@ -1,27 +1,27 @@
 /* Resources:
-*   [1] https://www.apollographql.com/docs/react/networking/advanced-http-networking/
-*   [2] Apollo Link Overview: https://www.apollographql.com/docs/react/api/link/introduction/
-*   [3] Why Apollo: https://www.apollographql.com/docs/react/why-apollo/
-*/
-import { createHttpLink } from 'apollo-link-http'
-import { ApolloLink, concat } from 'apollo-link';
+ *   [1] https://www.apollographql.com/docs/react/networking/advanced-http-networking/
+ *   [2] Apollo Link Overview: https://www.apollographql.com/docs/react/api/link/introduction/
+ *   [3] Why Apollo: https://www.apollographql.com/docs/react/why-apollo/
+ */
+import { createHttpLink } from "apollo-link-http";
+import { ApolloLink, concat } from "apollo-link";
 
-const ACCESS_TOKEN = localStorage.getItem("access-token") || "No Token"
-const TOKEN_NAME = "JWT"
+const ACCESS_TOKEN = localStorage.getItem("access-token") || "No Token";
+const TOKEN_NAME = "JWT";
 
 // HTTP connection to the API, replace with bud grapqhl server url
 const httpLink = createHttpLink({
-    uri: 'https://api.spacex.land/graphql/',
-})
+  uri: "https://api.spacex.land/graphql/",
+});
 
 // add the authorization to the request header
 const authLink = new ApolloLink((operation, forward) => {
-    operation.setContext({
-      headers: {
-        Authorization: `${TOKEN_NAME} ${ACCESS_TOKEN}`,
-      }
-    });
-    return forward(operation);
-})
-  
-export const customLinks = concat(authLink,httpLink)
+  operation.setContext({
+    headers: {
+      Authorization: `${TOKEN_NAME} ${ACCESS_TOKEN}`,
+    },
+  });
+  return forward(operation);
+});
+
+export const customLinks = concat(authLink, httpLink);
