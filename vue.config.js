db04a9c1e9fd9af73697aa35054b71cbf52262c9
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2021-05-12 14:37:11 +0800

    Fix bug gql loader not working

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+/* Resources:
+*   [1] Loader for gql: https://cli.vuejs.org/guide/webpack.html#modifying-options-of-a-loader
+*
+*/
+
 module.exports = {
+  chainWebpack: config => { // [1]
+    // GraphQL Loader
+    config.module
+      .rule('graphql')
+      .test(/\.gql$/)
+      .use('graphql-tag/loader')
+        .loader('graphql-tag/loader')
+        .end()
+  },
   transpileDependencies: ["vuetify"],
+
 };
