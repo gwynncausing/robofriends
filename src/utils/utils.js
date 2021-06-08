1a3b6553b75acb620f9efc2909c252d2d991af6f
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2021-06-08 15:08:21 +0800

    Add project statuses constants

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
+export const PROJECT_NOT_STARTED = "not_started";
+export const PROJECT_PENDING = "waiting_for_approval";
+export const PROJECT_ONGOING = "ongoing";
+export const PROJECT_FOR_REVISION = "for_revision";
+export const PROJECT_FINISHED = "finished";
+
 export const parseProjectStatus = (status) => {
   const statusCounterparts = {
     NOT_STARTED: "Not Started",
