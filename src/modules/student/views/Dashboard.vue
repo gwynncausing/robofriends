3c63d4c39d3a651f00244e30fc40673278e11049
gwynncausing <gwynn.causing23@gmail.com>
2021-09-29 15:31:59 +0800

    Update Dashboard no team
    added modal on join click
    change illustration

--- src/modules/student/views/Dashboard.vue
+++ src/modules/student/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <div id="dashboard">
     <!-- //* Remove negation to hasTeam to show team/join team -->
     <div v-if="!hasTeam" id="dashboard-no-team">
-      <v-img :src="require('@/assets/invitation.svg')" width="600" />
+      <v-img :src="require('@/assets/dashboard-no-team.svg')" width="400" />
       <div class="dashboard-cta">
         <h5>Looks like you don’t have a team yet.</h5>
         <p>
@@ -13,11 +13,34 @@
           </router-link>
           or
           <!-- // TODO:  change to go to team list -->
-          <router-link class="secondary--text" :to="{ name: 'CreateAccount' }">
+          <span
+            class="secondary--text btn-join-team"
+            @click="
+              {
+                joinTeamModal = true;
+              }
+            "
+          >
             <strong>join</strong>
-          </router-link>
+          </span>
           a team?
         </p>
+        <JoinTeamModal :dialog="joinTeamModal" @closed="joinTeamModal = $event">
+          <template v-slot:title>
+            <h4>Join a team</h4>
+            <span class="subheading1 neutral-500--text">
+              Enter the team code given by your friend
+            </span>
+          </template>
+          <template v-slot:content>
+            <TextField v-model="teamCode" placeholder="Team code"> </TextField>
+          </template>
+          <template v-slot:footer>
+            <v-spacer></v-spacer>
+            <Button text @click="joinTeamModal = false">Cancel</Button>
+            <Button @click="joinTeam()">Submit</Button>
+          </template>
+        </JoinTeamModal>
       </div>
     </div>
     <div v-else id="dashboard-has-team">
@@ -41,6 +64,9 @@ import TeamTracker from "./dashboard/TeamTracker";
 import TasksBoard from "./dashboard/TasksBoard";
 import IndividualInsight from "./dashboard/IndividualInsight";
 import AppTabs from "../components/AppTabs";
+import JoinTeamModal from "../components/JoinTeamModal";
+import TextField from "@/components/global/TextField.vue";
+import Button from "@/components/global/Button.vue";
 
 export default {
   name: "Dashboard",
@@ -49,14 +75,17 @@ export default {
     TasksBoard,
     IndividualInsight,
     AppTabs,
+    JoinTeamModal,
+    TextField,
+    Button,
   },
   data() {
     return {
-      dialog: true,
+      joinTeamModal: false,
       user: {
         teamList: {},
       },
-      hasTeam: true,
+      hasTeam: false,
       items: [
         {
           title: "Team Tracker",
@@ -71,7 +100,13 @@ export default {
           value: "tasks-board",
         },
       ],
+      teamCode: "",
     };
+  },
+  methods: {
+    joinTeam() {
+      console.log("joinTeam called ", this.teamCode);
+    },
   },
 };
 </script>
@@ -83,6 +118,10 @@ export default {
 
   #dashboard-no-team {
     padding: 40px 16px;
+
+    .btn-join-team {
+      cursor: pointer;
+    }
   }
   .dashboard-cta {
     margin-top: 48px;
