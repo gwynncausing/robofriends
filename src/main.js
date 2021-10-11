5a0b4957d459e2294693b2ac38084ef1b7e18079
rafalebacalla <raprap.bacalla@gmail.com>
2021-10-11 15:41:59 +0800

    add firebase boilerplate

--- src/main.js
+++ src/main.js
@@ -27,3 +27,25 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCfQHu1q5F9hG3GMB3W8hFElJmfpf3qI5s",
+  authDomain: "bud-dev-1531b.firebaseapp.com",
+  databaseURL: "https://bud-dev-1531b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bud-dev-1531b",
+  storageBucket: "bud-dev-1531b.appspot.com",
+  messagingSenderId: "803627261639",
+  appId: "1:803627261639:web:a20b11f063e7b114c0a8e2",
+  measurementId: "G-WYCCTDFFE5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
