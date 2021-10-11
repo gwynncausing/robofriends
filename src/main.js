5490641df3fc14df2f2b42076aa5628b71057cbf
rafalebacalla <raprap.bacalla@gmail.com>
2021-10-11 17:49:51 +0800

    add example code for collaboration

--- src/main.js
+++ src/main.js
@@ -39,12 +39,13 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyCfQHu1q5F9hG3GMB3W8hFElJmfpf3qI5s",
   authDomain: "bud-dev-1531b.firebaseapp.com",
-  databaseURL: "https://bud-dev-1531b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://bud-dev-1531b-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "bud-dev-1531b",
   storageBucket: "bud-dev-1531b.appspot.com",
   messagingSenderId: "803627261639",
   appId: "1:803627261639:web:a20b11f063e7b114c0a8e2",
-  measurementId: "G-WYCCTDFFE5"
+  measurementId: "G-WYCCTDFFE5",
 };
 
 // Initialize Firebase
