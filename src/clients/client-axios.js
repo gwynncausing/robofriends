ea6a54d3c1336db87c3ab594834fdea22b47854a
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2021-10-23 17:43:59 +0800

    Update bud server api url

--- src/clients/client-axios.js
+++ src/clients/client-axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { authInterceptor } from "./interceptors/auth-interceptor";
 
-const baseDomain = process.env.VUE_APP_BUD_API;
+const baseDomain = process.env.VUE_APP_BUD_API_HOSTED;
 const baseURL = `${baseDomain}`; // Incase of /api/v1;
 
 const clientAxios = axios.create({
