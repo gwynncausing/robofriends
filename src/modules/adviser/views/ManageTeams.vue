1d2df31ecbde14f2e7bacf5f069242e55669633e
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2021-10-24 19:26:02 +0800

    Add initial Choose Team

--- src/modules/adviser/views/ManageTeams.vue
+++ src/modules/adviser/views/ManageTeams.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="manage-teams">
+    <ChooseTeam :team="team" />
     <Tabs active="pending-proposals" :items="items" class="tabs">
       <template v-slot:body-pending-proposals>
         <PendingProposals />
@@ -16,9 +17,11 @@
 
 <script>
 import Tabs from "@/components/Tabs";
+
 import PendingProposals from "@/components/adviser/manage-teams/PendingProposals";
 import ApprovedResearch from "@/components/adviser/manage-teams/ApprovedResearch";
 import ResearchPaper from "@/components/adviser/manage-teams/ResearchPaper";
+import ChooseTeam from "@/components/adviser/manage-teams/ChooseTeam";
 export default {
   name: "ManageTeams",
   components: {
@@ -26,6 +29,7 @@ export default {
     PendingProposals,
     ApprovedResearch,
     ResearchPaper,
+    ChooseTeam,
   },
   data() {
     return {
@@ -43,6 +47,20 @@ export default {
           value: "research-paper",
         },
       ],
+      team: {
+        teamName: "Cary & Co.",
+        researchTitle:
+          "Bud: Gamified Research Management System with Real Time Collaboration and AutoFormatting",
+        members: [
+          "Cary Legaspi",
+          "Rafale Bacalla",
+          "Hyksos Gwynn Causing",
+          "Wylen Joan Lee",
+          "Joshua Rosalijos",
+          "Someone Else",
+        ],
+        dateTime: "09/05/2021 11:59AM",
+      },
     };
   },
 };
