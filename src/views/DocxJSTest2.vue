04b495d1d5f19410f73163ba5d2c250f97d694f4
gwynncausing <gwynn.causing23@gmail.com>
2021-11-10 22:37:53 +0800

    Add more sample in docx test 2

--- src/views/DocxJSTest2.vue
+++ src/views/DocxJSTest2.vue
@@ -4,6 +4,8 @@
 
 <script>
 import autoformat from "@/utils/autoformatter/autoformat";
+import sampleV6 from "@/utils/sample-v6.json"; // tables
+import sampleV7 from "@/utils/sample-v7.json"; // tables
 import sampleV9 from "@/utils/sample-v9.json"; // tables
 import { ACM_FORMAT } from "@/utils/autoformatter/format-rules";
 
@@ -15,7 +17,7 @@ export default {
   },
 
   mounted() {
-    autoformat.generateDocument(ACM_FORMAT, [sampleV9]);
+    autoformat.generateDocument(ACM_FORMAT, [sampleV6, sampleV7, sampleV9]);
   },
 };
 </script>
