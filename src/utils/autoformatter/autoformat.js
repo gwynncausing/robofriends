cdac241654d5e6c2b7fe75db78f625783dc35bcb
gwynncausing <gwynn.causing23@gmail.com>
2021-11-10 22:36:17 +0800

    Add table in autoformatter

--- src/utils/autoformatter/autoformat.js
+++ src/utils/autoformatter/autoformat.js
@@ -1,14 +1,22 @@
+/* eslint-disable */
+
 import {
   // AlignmentType,
   // LevelFormat,
   // Column,
   // PageOrientation,
   // HeadingLevel,
+  convertInchesToTwip,
   Paragraph,
+  TextRun,
   Document,
   Packer,
   ImageRun,
   SectionType,
+  Table,
+  TableCell,
+  TableRow,
+  WidthType,
 } from "docx";
 
 import { saveAs } from "file-saver";
@@ -16,7 +24,62 @@ import { createParagraph, createList } from "./text-block";
 import { createHeading, getHeadingNumber } from "./heading-block";
 
 // TODO: add implementation
-export const createTable = () => {};
+const createTableCellsChildren = (content) => {
+  let children = [];
+  for (const childContent of content) {
+    if (childContent.type === "paragraph") {
+      children.push(createParagraph(childContent.content));
+    } else if (
+      childContent.type === "orderedList" ||
+      childContent.type === "bulletList"
+    ) {
+      const results = createList(childContent.content, childContent.type);
+      results.forEach((result) => children.push(result));
+    }
+  }
+  return children;
+};
+
+const createTableCells = (content) => {
+  console.log("children:", createTableCellsChildren(content.content));
+  const tableCell = new TableCell({
+    children: createTableCellsChildren(content.content),
+    rowSpan: content.attrs.rowspan,
+    columnSpan: content.attrs.colspan,
+  });
+  return tableCell;
+};
+// TODO: add bold on tableHeader
+const createTableRows = (content) => {
+  let tableCells = [];
+  for (let childContent of content) {
+    if (childContent.type === "tableHeader")
+      tableCells.push(createTableCells(childContent));
+    if (childContent.type === "tableCell")
+      tableCells.push(createTableCells(childContent));
+  }
+
+  const tableRow = new TableRow({
+    children: tableCells,
+  });
+  return tableRow;
+};
+
+export const createTable = (content) => {
+  let tableRows = [];
+  for (let row of content) {
+    if (row.type !== "tableRow") break;
+    tableRows.push(createTableRows(row.content));
+  }
+  const table = new Table({
+    rows: tableRows,
+    width: {
+      size: 100,
+      type: WidthType.PERCENTAGE,
+    },
+  });
+  return table;
+};
 
 const getImageHeightWidth = (src) => {
   return new Promise((resolve, reject) => {
@@ -27,6 +90,8 @@ const getImageHeightWidth = (src) => {
   });
 };
 
+// column width 8.45cm converted to px
+// https://www.unitconverters.net/typography/centimeter-to-pixel-x.htm
 const createImageRun = async (imgUrl, targetWidth = 319.37007874) => {
   let height = 0,
     width = 0;
@@ -48,7 +113,7 @@ const createImageRun = async (imgUrl, targetWidth = 319.37007874) => {
   });
 };
 
-// TODO: handle 1 column images
+// TODO: add implementation
 export const createImage = async (url) => {
   const imagerun = await createImageRun(url);
   return new Paragraph({
@@ -78,15 +143,13 @@ const createSection = ({ document, children = [] }) => {
   };
 };
 
-export const createDocumentProperties = (
-  rules,
-  //TODO: add these from generated content in editor
-  { authors = "Unknown", title = "Untitled", description = "" }
-) => {
+//TODO: polish
+export const createDocumentProperties = (rules) => {
   const properties = {
-    creator: authors,
-    title: title,
-    description: description,
+    //TODO: should come from content object instead
+    creator: rules.creator,
+    title: rules.title,
+    description: rules.description,
     styles: {
       default: { ...rules.styles.headings },
       paragraphStyles: rules.styles.paragraphs,
@@ -99,17 +162,18 @@ export const createDocumentProperties = (
   return properties;
 };
 
+//TODO: polish text/heading block related functions
 export const generateDocument = async (rules, content) => {
   const numberList = [];
-  const properties = createDocumentProperties(rules, {});
+  const properties = createDocumentProperties(rules);
   const section = createSection({ document: rules.document, children: [] });
   // TODO: create a function called createSectionChildren and insert the code block below
   for (const item of content) {
     if (item.blockType === "section" || item.blockType === "heading") {
-      for (const innerContent of item.content) {
-        if (innerContent.type === "heading") {
-          let headingText = innerContent.content?.[0].text;
-          const level = innerContent.attrs.level;
+      for (const childContent of item.content) {
+        if (childContent.type === "heading") {
+          let headingText = childContent.content?.[0].text;
+          const level = childContent.attrs.level;
           if (rules.headingOptions.isNumbered) {
             // * add numbers to heading
             headingText = `${getHeadingNumber(
@@ -122,14 +186,14 @@ export const generateDocument = async (rules, content) => {
         }
       }
     } else if (item.blockType === "text") {
-      for (const innerContent of item.content) {
-        if (innerContent.type === "paragraph") {
-          section.children.push(createParagraph(innerContent.content));
+      for (const childContent of item.content) {
+        if (childContent.type === "paragraph") {
+          section.children.push(createParagraph(childContent.content));
         } else if (
-          innerContent.type === "orderedList" ||
-          innerContent.type === "bulletList"
+          childContent.type === "orderedList" ||
+          childContent.type === "bulletList"
         ) {
-          const results = createList(innerContent.content, innerContent.type);
+          const results = createList(childContent.content, childContent.type);
           results.forEach((result) => section.children.push(result));
         }
       }
@@ -139,17 +203,17 @@ export const generateDocument = async (rules, content) => {
       if (tempContent[0].type === "heading")
         tempContent = item.content.reverse();
 
-      for (const innerContent of tempContent) {
-        if (innerContent.type === "image") {
-          const result = await createImage(innerContent.attrs.src);
+      for (const childContent of tempContent) {
+        if (childContent.type === "image") {
+          const result = await createImage(childContent.attrs.src);
           section.children.push(result);
-        } else if (innerContent.type === "heading") {
+        } else if (childContent.type === "heading") {
           // * Added due to TextRun probably overrides the bold in paragraph styling
           let tempContentText = [
             {
               type: "text",
               marks: [{ type: "bold" }],
-              text: innerContent.content[0].text,
+              text: childContent.content[0].text,
             },
           ];
 
@@ -158,6 +222,23 @@ export const generateDocument = async (rules, content) => {
           );
         }
       }
+    } else if (item.blockType === "table") {
+      for (const childContent of item.content) {
+        if (childContent.type !== "table") break;
+        let table = createTable(childContent.content);
+        section.children.push(table);
+      }
+      // const table = new Table({
+      //   rows: [
+      //     new TableRow({
+      //       children: [
+      //         new TableCell({
+      //           children: [new Paragraph({})],
+      //         }),
+      //       ],
+      //     }),
+      //   ],
+      // });
     }
 
     // for (const [innerIndex, innerContent] of item.content.entries()) {
--- src/views/DocxJSTest2.vue
+++ src/views/DocxJSTest2.vue
@@ -1,25 +1,13 @@
 <template>
-  <div>DocxJSTestStyling</div>
+  <div>Exports from autoformatter</div>
 </template>
 
 <script>
-import {
-  AlignmentType,
-  // convertInchesToTwip,
-  Document,
-  HeadingLevel,
-  // LevelFormat,
-  Packer,
-  Paragraph,
-  TextRun,
-  Column,
-  PageOrientation,
-  // UnderlineType,
-} from "docx";
-import { saveAs } from "file-saver";
+import autoformat from "@/utils/autoformatter/autoformat";
+import sampleV9 from "@/utils/sample-v9.json"; // tables
+import { ACM_FORMAT } from "@/utils/autoformatter/format-rules";
 
 export default {
-  name: "DocxJSTestStyling",
   data() {
     return {
       //
@@ -27,131 +15,7 @@ export default {
   },
 
   mounted() {
-    const properties = {
-      creator: "Clippy",
-      title: "Sample Document",
-      description: "A brief example of using docx",
-      styles: {
-        default: {
-          //* HEADING 1 - OK
-          heading1: {
-            run: {
-              size: "12pt",
-              bold: true,
-              allCaps: true,
-              color: "000000",
-            },
-            paragraph: {
-              alignment: AlignmentType.LEFT,
-              spacing: {
-                before: "6pt",
-              },
-            },
-          },
-          //* HEADING 2 - Capitalized not found
-          heading2: {
-            run: {
-              size: "12pt",
-              bold: true,
-            },
-            paragraph: {
-              alignment: AlignmentType.LEFT,
-              spacing: {
-                before: "6pt",
-              },
-            },
-          },
-        },
-        paragraphStyles: [
-          //* DEFAULT PARAGRAPH - OK
-          {
-            id: "Normal",
-            name: "Normal",
-            run: {
-              font: "Times New Roman",
-              size: "9pt",
-              color: "#000000",
-            },
-            paragraph: {
-              alignment: AlignmentType.JUSTIFIED,
-            },
-          },
-        ],
-      },
-      sections: [
-        {
-          properties: {
-            //* DOCUMENT RULES
-            page: {
-              size: {
-                orientation: PageOrientation.PORTRAIT,
-                height: "27.94cm",
-                width: "21.59cm",
-              },
-              margin: {
-                top: "1.9cm",
-                right: "1.9cm",
-                bottom: "2.54cm",
-                left: "1.9cm",
-              },
-            },
-            //* PAGE COLUMNS
-            column: {
-              count: 2,
-              space: ".83cm",
-              equalWidth: true,
-              children: [
-                new Column({ width: "8.45cm" }),
-                new Column({ width: "8.45cm" }),
-              ],
-            },
-          },
-          children: [
-            new Paragraph({
-              text: "Test heading1, bold and italicized",
-              heading: HeadingLevel.HEADING_1,
-            }),
-            new Paragraph({
-              text: "Some simple content",
-              style: "Normal",
-            }),
-            new Paragraph({
-              text: "Test heading2 with double red underline",
-              heading: HeadingLevel.HEADING_2,
-            }),
-            new Paragraph({
-              children: [
-                new TextRun({
-                  text: "Some monospaced content",
-                  font: {
-                    name: "Monospace",
-                  },
-                }),
-              ],
-            }),
-            new Paragraph({
-              text: "An aside, in light gray italics and indented",
-              style: "aside",
-            }),
-            new Paragraph({
-              text: "This is normal, but well-spaced text",
-              style: "wellSpaced",
-            }),
-            new Paragraph({
-              text: "Test heading2 with double red underline and wellSpaced",
-              // heading: HeadingLevel.HEADING_2,
-              style: "wellSpacedHeading",
-            }),
-          ],
-        },
-      ],
-    };
-    console.log(properties);
-    const doc = new Document(properties);
-
-    Packer.toBlob(doc).then((blob) => {
-      saveAs(blob, "test.docx");
-    });
+    autoformat.generateDocument(ACM_FORMAT, [sampleV9]);
   },
 };
 </script>
