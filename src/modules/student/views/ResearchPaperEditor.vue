8b5e43a8dd7f788083170b7ebc48a77b20243ec7
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2021-11-10 16:51:52 +0800

    Implement EditorDraggable in ResearchPaperEditor

--- src/modules/student/views/ResearchPaperEditor.vue
+++ src/modules/student/views/ResearchPaperEditor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="editor">
+  <!-- <div id="editor">
     <div class="editor-heading">
       <Button text class="neutral-800--text mr-auto">Version History</Button>
       <ActiveUsersList :users="activeUsers" class="mr-4" />
@@ -67,24 +67,114 @@
         @removeEditor="removeEditor($event)"
       />
     </div>
+  </div> -->
+  <div id="editor">
+    <div class="editor-heading">
+      <Button text class="neutral-800--text mr-auto">Version History</Button>
+      <ActiveUsersList :users="activeUsers" class="mr-4" />
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <Button outlined v-bind="attrs" v-on="on">
+            Export
+            <v-icon>mdi-chevron-down</v-icon>
+          </Button>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in exportItems" :key="index" link>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+    <div class="editor-list-wrapper">
+      <div class="editor-list">
+        <div class="editor-row">
+          <EditorDraggable
+            :list="editors"
+            :user-color="userColor"
+            :users="activeUsers"
+          />
+        </div>
+
+        <EditorToolbar
+          :current-toolbar-position="currentToolbarPosition"
+          :current-selected-editor-index="currentSelectedEditorIndex"
+          @addEditor="addEditor($event)"
+          @removeEditor="removeEditor($event)"
+        />
+      </div>
+    </div>
+
+    <!-- <div class="editor-list-wrapper">
+      <div class="editor-list">
+        <div
+          v-for="(editor, index) in editors"
+          :id="'editor-' + editor.id"
+          :key="editor.id"
+          class="editor-row"
+        >
+          <div v-if="editor.blockType === 'text'" class="editor-content-text">
+            <EditorText
+              :editor-data="editor"
+              :user-color="userColor"
+              @input="getContent($event, index)"
+              @updateUsers="updateUsers($event)"
+              @selectBlock="selectBlock($event)"
+            />
+          </div>
+          <div
+            v-else-if="editor.blockType === 'image'"
+            class="editor-content-image"
+          >
+            <EditorImage
+              :editor-data="editor"
+              :user-color="userColor"
+              @input="getContent($event, index)"
+              @updateUsers="updateUsers($event)"
+              @selectBlock="selectBlock($event)"
+            />
+          </div>
+          <div
+            v-else-if="editor.blockType === 'section'"
+            class="editor-content-section"
+          >
+            <EditorSection
+              :editor-data="editor"
+              :user-color="userColor"
+              @input="getContent($event, index)"
+              @updateUsers="updateUsers($event)"
+              @selectBlock="selectBlock($event)"
+            />
+          </div>
+        </div>
+      </div>
+      <EditorToolbar
+        :current-toolbar-position="currentToolbarPosition"
+        :current-selected-editor-index="currentSelectedEditorIndex"
+        @addEditor="addEditor($event)"
+        @removeEditor="removeEditor($event)"
+      />
+    </div> -->
   </div>
 </template>
 
 <script>
+import EditorDraggable from "@/components/EditorDraggable.vue";
 import Button from "@/components/global/Button.vue";
-import EditorText from "@/components/editor/EditorText.vue";
-import EditorImage from "@/components/editor/EditorImage.vue";
-import EditorSection from "@/components/editor/EditorSection.vue";
+// import EditorText from "@/components/editor/EditorText.vue";
+// import EditorImage from "@/components/editor/EditorImage.vue";
+// import EditorSection from "@/components/editor/EditorSection.vue";
 import EditorToolbar from "@/components/editor/EditorToolbar.vue";
 import ActiveUsersList from "@/components/editor/ActiveUsersList.vue";
 
 export default {
   name: "ResearchPaperEditor",
   components: {
+    EditorDraggable,
     Button,
-    EditorText,
-    EditorImage,
-    EditorSection,
+    // EditorText,
+    // EditorImage,
+    // EditorSection,
     EditorToolbar,
     ActiveUsersList,
   },
@@ -117,35 +207,36 @@ export default {
     updateUsers(users) {
       this.activeUsers = users;
     },
-    selectBlock(object) {
-      const index = this.editors.map((editor) => editor.id).indexOf(object.id);
-      this.moveToolbar(object.id, index);
-      this.currentSelectedEditorIndex = index;
-    },
-    moveToolbar(id, index) {
-      const editorID = "editor-" + id;
-      let position = 0;
+    // selectBlock(object) {
+    //   const index = this.editors.map((editor) => editor.id).indexOf(object.id);
+    //   this.moveToolbar(object.id, index);
+    //   this.currentSelectedEditorIndex = index;
+    // },
+    // moveToolbar(id, index) {
+    //   const editorID = "editor-" + id;
+    //   let position = 0;
 
-      for (let i = 0; i < this.editors.length; i++) {
-        let editorID = "editor-" + this.editors[i].id;
-        position += document.getElementById(editorID).clientHeight;
-        if (i === index) break;
-        position += 24;
-      }
+    //   for (let i = 0; i < this.editors.length; i++) {
+    //     let editorID = "editor-" + this.editors[i].id;
+    //     position += document.getElementById(editorID).clientHeight;
+    //     if (i === index) break;
+    //     position += 24;
+    //   }
 
-      const blockHeight = document.getElementById(editorID).clientHeight;
-      this.currentToolbarPosition = position - blockHeight;
-    },
-    getContent(event, index) {
-      this.editors[index].content = event.content;
-    },
-    addEditor({ currentSelectedEditorIndex: index, blockType = "text" }) {
+    //   const blockHeight = document.getElementById(editorID).clientHeight;
+    //   this.currentToolbarPosition = position - blockHeight;
+    // },
+    // getContent(event, index) {
+    //   this.editors[index].content = event.content;
+    // },
+    addEditor({ currentSelectedEditorIndex: index, blockType = "section" }) {
       if (index === -1) return;
       this.editors.splice(index + 1, 0, {
         id: this.id++,
         content: ``,
         blockType,
         users: [],
+        children: [],
       });
     },
     removeEditor({ currentSelectedEditorIndex: index = -1 }) {
@@ -189,24 +280,26 @@ export default {
     .editor-row {
       display: flex;
       gap: 16px;
-
-      div[class^="editor-content-"] {
-        width: 100%;
-        border: 1px solid $neutral-400;
-        border-radius: 4px;
-        padding: 0.8rem;
-        margin-right: 76px;
-      }
-      .editor-content-section {
-        height: 42px;
-        padding: 4px;
-      }
-
-      .editor-content-text,
-      .editor-content-image {
-        min-height: 18rem; // 304px
-      }
+      margin-right: 76px;
     }
+
+    //   div[class^="editor-content-"] {
+    //     width: 100%;
+    //     border: 1px solid $neutral-400;
+    //     border-radius: 4px;
+    //     padding: 0.8rem;
+    //     margin-right: 76px;
+    //   }
+    //   .editor-content-section {
+    //     height: 42px;
+    //     padding: 4px;
+    //   }
+
+    //   .editor-content-text,
+    //   .editor-content-image {
+    //     min-height: 18rem; // 304px
+    //   }
+    // }
   }
 }
 </style>
