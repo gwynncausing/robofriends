7c88ea851895a9f3e6c51387b57ee3dceb02c47a
gwynncausing <gwynn.causing23@gmail.com>
2021-11-11 19:07:19 +0800

    Update tiptap dependencies and Add Text Font Alignment

--- src/components/editor/EditorText.vue
+++ src/components/editor/EditorText.vue
@@ -27,6 +27,7 @@ import Table from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
+import TextAlign from "@tiptap/extension-text-align";
 
 import Collaboration from "@tiptap/extension-collaboration";
 import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
@@ -120,6 +121,9 @@ export default {
           Superscript,
           Subscript,
           Image,
+          TextAlign.configure({
+            types: ["paragraph"],
+          }),
           Table.configure({
             resizable: true,
           }),
--- src/components/editor/EditorTextFormatterButtons.vue
+++ src/components/editor/EditorTextFormatterButtons.vue
@@ -8,43 +8,76 @@
         hidden
         @change="selectFiles"
       />
-      <button @click="addImage">
+      <button title="Add Image" @click="addImage">
         <v-icon>mdi-image-plus</v-icon>
       </button>
     </div>
 
     <div v-else-if="blockType === 'text' || blockType === 'table'">
       <button
+        title="Align Left"
+        :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }"
+        @click="editor.chain().focus().setTextAlign('left').run()"
+      >
+        <v-icon>mdi-format-align-left</v-icon>
+      </button>
+      <button
+        title="Align Center"
+        :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }"
+        @click="editor.chain().focus().setTextAlign('center').run()"
+      >
+        <v-icon>mdi-format-align-center</v-icon>
+      </button>
+      <button
+        title="Align Right"
+        :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }"
+        @click="editor.chain().focus().setTextAlign('right').run()"
+      >
+        <v-icon>mdi-format-align-right</v-icon>
+      </button>
+      <button
+        :class="{ 'is-active': editor.isActive({ textAlign: 'justify' }) }"
+        @click="editor.chain().focus().setTextAlign('justify').run()"
+      >
+        <v-icon>mdi-format-align-justify</v-icon>
+      </button>
+      <button
+        title="Bold"
         :class="{ 'is-active': editor.isActive('bold') }"
         @click="editor.chain().focus().toggleBold().run()"
       >
         <v-icon> mdi-format-bold </v-icon>
       </button>
       <button
+        title="Italic"
         :class="{ 'is-active': editor.isActive('italic') }"
         @click="editor.chain().focus().toggleItalic().run()"
       >
         <v-icon> mdi-format-italic </v-icon>
       </button>
       <button
+        title="Underline"
         :class="{ 'is-active': editor.isActive('underline') }"
         @click="editor.chain().focus().toggleUnderline().run()"
       >
         <v-icon> mdi-format-underline </v-icon>
       </button>
       <button
+        title="Strikethrough"
         :class="{ 'is-active': editor.isActive('strike') }"
         @click="editor.chain().focus().toggleStrike().run()"
       >
         <v-icon> mdi-format-strikethrough-variant </v-icon>
       </button>
       <button
+        title="Subscript"
         :class="{ 'is-active': editor.isActive('subscript') }"
         @click="toggleSubscript()"
       >
         <v-icon> mdi-format-subscript </v-icon>
       </button>
       <button
+        title="Superscript"
         :class="{ 'is-active': editor.isActive('superscript') }"
         @click="toggleSuperscript()"
       >
@@ -54,12 +87,14 @@
       <span class="formatter-section-end"></span>
 
       <button
+        title="Bullet List"
         :class="{ 'is-active': editor.isActive('bulletList') }"
         @click="editor.chain().focus().toggleBulletList().run()"
       >
         <v-icon>mdi-format-list-bulleted</v-icon>
       </button>
       <button
+        title="Number List"
         :class="{ 'is-active': editor.isActive('orderedList') }"
         @click="editor.chain().focus().toggleOrderedList().run()"
       >
@@ -68,13 +103,14 @@
 
       <span class="formatter-section-end"></span>
 
-      <button @click="editor.chain().focus().redo().run()">
+      <button title="Redo" @click="editor.chain().focus().redo().run()">
         <v-icon>mdi-redo</v-icon>
       </button>
-      <button @click="editor.chain().focus().undo().run()">
+      <button title="Undo" @click="editor.chain().focus().undo().run()">
         <v-icon>mdi-undo</v-icon>
       </button>
       <button
+        title="Clear Formatting"
         @click="
           {
             editor.chain().focus().clearNodes().run(),
@@ -87,17 +123,6 @@
     </div>
 
     <div v-if="blockType === 'table'">
-      <button
-        @click="
-          editor
-            .chain()
-            .focus()
-            .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
-            .run()
-        "
-      >
-        insertTable
-      </button>
       <button
         :disabled="!editor.can().addColumnBefore()"
         @click="editor.chain().focus().addColumnBefore().run()"
@@ -135,12 +160,6 @@
         deleteRow
       </button>
       <button
-        :disabled="!editor.can().deleteTable()"
-        @click="editor.chain().focus().deleteTable().run()"
-      >
-        deleteTable
-      </button>
-      <button
         :disabled="!editor.can().mergeCells()"
         @click="editor.chain().focus().mergeCells().run()"
       >
@@ -151,24 +170,6 @@
         @click="editor.chain().focus().splitCell().run()"
       >
         splitCell
-      </button>
-      <button
-        :disabled="!editor.can().toggleHeaderColumn()"
-        @click="editor.chain().focus().toggleHeaderColumn().run()"
-      >
-        toggleHeaderColumn
-      </button>
-      <button
-        :disabled="!editor.can().toggleHeaderRow()"
-        @click="editor.chain().focus().toggleHeaderRow().run()"
-      >
-        toggleHeaderRow
-      </button>
-      <button
-        :disabled="!editor.can().toggleHeaderCell()"
-        @click="editor.chain().focus().toggleHeaderCell().run()"
-      >
-        toggleHeaderCell
       </button>
     </div>
 
