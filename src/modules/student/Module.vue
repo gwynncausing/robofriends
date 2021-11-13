ead54f884aaf3d43a9fb79765f822c2e4ca79f9e
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2021-11-13 12:33:18 +0800

    Create archive view in student and adviser module

--- src/modules/student/Module.vue
+++ src/modules/student/Module.vue
@@ -58,7 +58,7 @@ export default {
         },
         {
           name: "Archive",
-          path: { name: "Archive" },
+          path: { name: "Student Archive" },
         },
       ],
       teamRoutes: [
