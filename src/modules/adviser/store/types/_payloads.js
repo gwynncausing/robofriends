da62c3e66cb510c1dce0583c9069bf1115011089
gwynncausing <gwynn.causing23@gmail.com>
2021-11-14 17:55:55 +0800

    Add pending proposal buttons functionality

--- src/modules/adviser/store/types/_payloads.js
+++ src/modules/adviser/store/types/_payloads.js
@@ -13,13 +13,26 @@ const FETCH_PENDING_PROPOSALS = {
   id: Number,
 };
 
+const FETCH_APPROVED_PROPOSAL = {
+  id: Number,
+};
+
 const FETCH_ONE_PENDING_PROPOSAL = {
   id: Number,
+};
+
+const UPDATE_PROPOSAL = {
+  id: Number,
+  status: String,
+  feedback: Object,
+  teamId: Number,
 };
 
 export const ADVISER_PAYLOADS = {
   UPDATE_INVITATION,
   FETCH_TEAM,
   FETCH_PENDING_PROPOSALS,
+  FETCH_APPROVED_PROPOSAL,
   FETCH_ONE_PENDING_PROPOSAL,
+  UPDATE_PROPOSAL,
 };
