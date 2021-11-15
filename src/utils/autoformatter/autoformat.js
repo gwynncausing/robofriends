580f4906da34b8334d0be8150cf3eb626a53b407
Cary Legaspi <legaspicary@gmail.com>
2021-11-15 15:02:39 +0800

    Refactor autoformat.js

--- src/utils/autoformatter/autoformat.js
+++ src/utils/autoformatter/autoformat.js
@@ -11,69 +11,11 @@ import {
 } from "docx";
 
 import { saveAs } from "file-saver";
-import { processTextBlock, createParagraph, createList } from "./text-block";
+import { processTextBlock } from "./text-block";
 import { processHeadingBlock } from "./heading-block";
 import { createACMCopyrightSpace } from "./special-elements";
 import { processImageBlock } from "./image-block";
-
-// TODO: add implefmentation
-const createTableCellsChildren = (content) => {
-  let children = [];
-  for (const childContent of content) {
-    if (childContent.type === "paragraph") {
-      children.push(createParagraph(childContent.content));
-    } else if (
-      childContent.type === "orderedList" ||
-      childContent.type === "bulletList"
-    ) {
-      const results = createList(childContent.content, childContent.type);
-      results.forEach((result) => children.push(result));
-    }
-  }
-  return children;
-};
-
-const createTableCells = (content) => {
-  const tableCell = new TableCell({
-    children: createTableCellsChildren(content.content),
-    rowSpan: content.attrs.rowspan,
-    columnSpan: content.attrs.colspan,
-  });
-  return tableCell;
-};
-// TODO: add bold on tableHeader
-const createTableRows = (content) => {
-  let tableCells = [];
-  for (let childContent of content) {
-    if (childContent.type === "tableHeader")
-      tableCells.push(createTableCells(childContent));
-    if (childContent.type === "tableCell")
-      tableCells.push(createTableCells(childContent));
-  }
-
-  const tableRow = new TableRow({
-    children: tableCells,
-  });
-  return tableRow;
-};
-
-export const createTable = (content) => {
-  let tableRows = [];
-  for (let row of content) {
-    if (row.type !== "tableRow") break;
-    tableRows.push(createTableRows(row.content));
-  }
-  const table = new Table({
-    rows: tableRows,
-    width: {
-      size: 100,
-      type: WidthType.PERCENTAGE,
-    },
-    // TODO: transfer to rules as default cell margins, also allow users to specify this if necessary
-    margins: {top: "0.05cm", right: "0.2cm", bottom: "0.05cm",  left: "0.1cm"}
-  });
-  return table;
-};
+import { processTableBlock } from "./table-block";
 
 //*OK
 export const createSectionProperties = ({ documentOptions, type }) => {
@@ -142,12 +84,7 @@ export const generateDocument = async (rules, content) => {
         section = await processImageBlock(rules, item, properties, section);
         break;
       case "table":
-        // TODO: add to separate file
-        for (const childContent of item.content) {
-          if (childContent.type !== "table") break;
-          let table = createTable(childContent.content);
-          section.children.push(table);
-        }
+        processTableBlock(item, section);
         break;
     }
   }
