b4b9f149d2b3ab21fdf4e2f4e098cf7739896999
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2021-11-22 15:30:53 +0800

    Fix styling for EditorDraggable, add dashed line to indicate children area

--- src/components/editor/EditorDraggable.vue
+++ src/components/editor/EditorDraggable.vue
@@ -13,89 +13,87 @@
       :key="editor.id"
       class="editor-panels-wrapper"
     >
-      <div class="parent-wrapper">
-        <div class="parent">
-          <v-icon class="handle"> mdi-drag-vertical </v-icon>
-          <v-btn
-            v-if="editor.blockType === 'heading'"
-            :id="'toggle-' + editor.id"
-            icon
-            class="toggle"
-            @click="toggleChildren(editor.id)"
-          >
-            <v-icon> mdi-chevron-right </v-icon>
-          </v-btn>
-          <div v-if="editor.blockType === 'text'" class="editor-content-text">
-            <EditorText
-              :editor-data="editor"
-              :user-color="userColor"
-              @input="input($event, index)"
-              @updateUsers="$emit('updateUsers', $event)"
-              @selectBlock="$emit('selectBlock', $event)"
-            />
-          </div>
-          <div
-            v-else-if="editor.blockType === 'image'"
-            class="editor-content-image"
-          >
-            <EditorImage
-              :editor-data="editor"
-              :user-color="userColor"
-              :column="editor.column"
-              @setColumn="
-                $emit('setColumn', {
-                  column: $event,
-                  editor: editor,
-                })
-              "
-              @input="input($event, index)"
-              @updateUsers="$emit('updateUsers', $event)"
-              @selectBlock="$emit('selectBlock', $event)"
-            />
-          </div>
+      <div class="parent">
+        <v-icon class="handle"> mdi-drag-vertical </v-icon>
+        <v-btn
+          v-if="editor.blockType === 'heading'"
+          :id="'toggle-' + editor.id"
+          icon
+          class="toggle"
+          @click="toggleChildren(editor.id)"
+        >
+          <v-icon> mdi-chevron-right </v-icon>
+        </v-btn>
+        <div v-if="editor.blockType === 'text'" class="editor-content-text">
+          <EditorText
+            :editor-data="editor"
+            :user-color="userColor"
+            @input="input($event, index)"
+            @updateUsers="$emit('updateUsers', $event)"
+            @selectBlock="$emit('selectBlock', $event)"
+          />
+        </div>
+        <div
+          v-else-if="editor.blockType === 'image'"
+          class="editor-content-image"
+        >
+          <EditorImage
+            :editor-data="editor"
+            :user-color="userColor"
+            :column="editor.column"
+            @setColumn="
+              $emit('setColumn', {
+                column: $event,
+                editor: editor,
+              })
+            "
+            @input="input($event, index)"
+            @updateUsers="$emit('updateUsers', $event)"
+            @selectBlock="$emit('selectBlock', $event)"
+          />
+        </div>
 
-          <div
-            v-else-if="editor.blockType === 'heading'"
-            class="editor-content-heading"
-          >
-            <EditorHeading
-              :editor-data="editor"
-              :user-color="userColor"
-              @input="input($event, index)"
-              @updateUsers="$emit('updateUsers', $event)"
-              @selectBlock="$emit('selectBlock', $event)"
-            />
-          </div>
-          <!-- @input="$emit('getContent', $event, index)" -->
-
-          <div
-            v-else-if="editor.blockType === 'table'"
-            class="editor-content-table"
-          >
-            <EditorTable
-              :editor-data="editor"
-              :user-color="userColor"
-              :column="editor.column"
-              @setColumn="
-                $emit('setColumn', {
-                  column: $event,
-                  editor: editor,
-                })
-              "
-              @input="input($event, index)"
-              @updateUsers="$emit('updateUsers', $event)"
-              @selectBlock="$emit('selectBlock', $event)"
-            />
-          </div>
+        <div
+          v-else-if="editor.blockType === 'heading'"
+          class="editor-content-heading"
+        >
+          <EditorHeading
+            :editor-data="editor"
+            :user-color="userColor"
+            @input="input($event, index)"
+            @updateUsers="$emit('updateUsers', $event)"
+            @selectBlock="$emit('selectBlock', $event)"
+          />
+        </div>
+        <div
+          v-else-if="editor.blockType === 'table'"
+          class="editor-content-table"
+        >
+          <EditorTable
+            :editor-data="editor"
+            :user-color="userColor"
+            :column="editor.column"
+            @setColumn="
+              $emit('setColumn', {
+                column: $event,
+                editor: editor,
+              })
+            "
+            @input="input($event, index)"
+            @updateUsers="$emit('updateUsers', $event)"
+            @selectBlock="$emit('selectBlock', $event)"
+          />
         </div>
       </div>
 
-      <div
-        v-show="editor.blockType === 'heading'"
-        :id="'children-' + editor.id"
-        class="children"
-      >
-        <EditorDraggable :list="editor.children" />
+      <div :id="'children-wrapper' + editor.id" class="children-wrapper">
+        <div
+          v-show="editor.blockType === 'heading'"
+          :id="'children-' + editor.id"
+          class="children"
+        >
+          <EditorDraggable :list="editor.children" />
+        </div>
       </div>
     </div>
   </draggable>
@@ -175,44 +173,49 @@ export default {
 .editor-panels-wrapper {
   background-color: white;
 }
-.parent-wrapper {
-  .parent {
-    display: flex;
-    align-items: top;
-    background-color: white;
-    padding-bottom: 24px;
-    .handle {
-      cursor: grab;
-      width: 24px;
-      margin-bottom: auto;
-      margin-top: 8px;
-    }
-    .toggle {
-      &:hover {
-        background-color: $neutral-50;
-      }
-    }
-    div[class^="editor-content-"] {
-      border: 1px solid $neutral-400;
-      border-radius: 4px;
-      padding: 0.8rem;
-      cursor: text;
-      width: 100%;
-    }
 
-    .editor-content-table,
-    .editor-content-text,
-    .editor-content-image {
-      margin-left: 36px;
+.parent {
+  display: flex;
+  align-items: top;
+  padding-bottom: 12px;
+  .handle {
+    cursor: grab;
+    width: 24px;
+    margin-bottom: auto;
+    margin-top: 8px;
+  }
+  .toggle {
+    &:hover {
+      background-color: $neutral-50;
     }
   }
+  div[class^="editor-content-"] {
+    border: 1px solid $neutral-400;
+    border-radius: 4px;
+    padding: 0.8rem;
+    cursor: text;
+    width: 100%;
+  }
+
+  .editor-content-table,
+  .editor-content-text,
+  .editor-content-image {
+    margin-left: 36px;
+  }
 }
-.children {
-  min-height: 200px;
-  height: fit-content;
-  margin-left: 50px;
-  display: none;
+.children-wrapper {
+  margin-left: 60px;
+  padding-bottom: 12px;
+  .children {
+    min-height: 200px;
+    height: fit-content;
+    display: none;
+    border: 1px dashed $neutral-400;
+    border-radius: 4px;
+    padding: 0.8rem;
+  }
 }
+
 .down {
   -moz-transform: rotate(90deg);
   -webkit-transform: rotate(90deg);
