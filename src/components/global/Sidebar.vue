08f8b5c01d41873665315ec402c79d47ff34b09c
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2021-11-24 17:14:37 +0800

    Add Sidebar component and used it in version history pages

---
+++ src/components/global/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="sidebar">
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#sidebar {
+  flex: 1;
+  border-left: 1px solid $neutral-400;
+  overflow-y: auto;
+  height: 100vh;
+  .title {
+    border-bottom: 1px solid $neutral-400;
+    padding: 16px;
+  }
+}
+</style>
