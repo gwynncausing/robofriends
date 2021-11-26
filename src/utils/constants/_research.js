481425d923c9a03bd3fbfdc06135294693e1d752
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2021-11-26 12:34:26 +0800

    Add research status constants

---
+++ src/utils/constants/_research.js
@@ -0,0 +1,9 @@
+const STATUS = {
+  ONGOING: "ongoing",
+  COMPLETED: "completed",
+  PUBLISHED: "published",
+};
+
+export const RESEARCH = {
+  STATUS,
+};
--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -2,4 +2,5 @@ export { STATUS_CODES } from "./_http-status-codes.js";
 export { USER } from "./_user.js";
 export { TEAM } from "./_team.js";
 export { PROPOSAL } from "./_proposal.js";
+export { RESEARCH } from "./_research.js";
 export { MODULES } from "./_modules.js";
