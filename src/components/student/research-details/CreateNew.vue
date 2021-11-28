7f82cb43031c1ef4173040455ee021e28b01f6bd
gwynncausing <gwynn.causing23@gmail.com>
2021-11-28 21:44:30 +0800

    missing random color

--- src/components/student/research-details/CreateNew.vue
+++ src/components/student/research-details/CreateNew.vue
@@ -127,6 +127,14 @@ export default {
     getTitle() {
       return this.editor.content?.content?.[0]?.content?.[0]?.text;
     },
+    getRandomColor() {
+      let letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
     updateUsers(users) {
       this.activeUsers = users;
       this.$emit("createNewProposalUsers", this.activeUsers.length);
